fcf9458f5b6229a0151a1a910c664288689bb065
Leewel <leewelkarani@gmail.com>
2019-10-16 21:29:30 +0300

    Configure migrations into application

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -8,17 +9,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://leewel@localhost:5432/todoap
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 
 class ToDo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
-
-db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
