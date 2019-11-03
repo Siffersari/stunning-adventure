ad6d5b75430de1a7539d95fc9cdf592d42c12719
Leewel Karani <leewelkarani@gmail.com>
2019-11-03 13:03:08 +0300

    Add complete todo List code and migrations

--- app.py
+++ app.py
@@ -17,9 +17,17 @@ class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('ToDo', backref='list', lazy=True)
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -78,6 +86,13 @@ def deleteTodo(todo_id):
 
     return redirect(url_for('index'))
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.htm',
+    lists=TodoList.query.all(),
+    active_list= TodoList.query.get(list_id),
+     todos=ToDo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.htm', data=ToDo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
